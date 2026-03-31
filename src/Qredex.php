984ca7f766cfec9976bf1d1e4f23f73b0ea48cd6
bobai <bobai@duck.com>
2026-03-31 10:09:39 +0100

    Bump SDK version to 0.1.1

--- src/Qredex.php
+++ src/Qredex.php
@@ -43,7 +43,7 @@ use Qredex\Resource\RefundsClient;
 
 final readonly class Qredex
 {
-    public const SDK_VERSION = '0.1.0';
+    public const SDK_VERSION = '0.1.1';
 
     private CreatorsClient $creators;
     private LinksClient $links;
